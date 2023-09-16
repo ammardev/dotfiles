18b8a34eda2840ce386638c8211720c01b8520ad
Ammar Al-Khawaldeh <me@ammar.dev>
2023-09-16 04:13:11 +0300

    Install treesitter text objects

--- nvim/lua/default/plugins.lua
+++ nvim/lua/default/plugins.lua
@@ -27,6 +27,10 @@ require("lazy").setup({
         end
     },
     { "nvim-treesitter/nvim-treesitter", build = "TSUpdate"},
+    {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      dependencies = "nvim-treesitter/nvim-treesitter",
+    },
     { "tpope/vim-fugitive" },
     {
         "airblade/vim-gitgutter",
