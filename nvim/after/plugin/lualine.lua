e6541bc202d0aa3e455f74cd1ebebe86aeb13d80
Ammar Al-Khawaldeh <me@ammar.dev>
2023-09-29 21:26:27 +0300

    Remove branch name because it's added now in tmux

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -19,7 +19,7 @@ require('lualine').setup {
     },
     sections = {
         lualine_a = {'buffers'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_b = {'diff', 'diagnostics'},
         lualine_c = {},
         lualine_x = {'filetype'},
         lualine_y = {'progress'},
