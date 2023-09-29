e7513b89f27fb81f04a473f1d2107da6d356a764
Ammar Al-Khawaldeh <me@ammar.dev>
2023-09-29 21:29:38 +0300

    Disable copy sync
    It's breaking the usage of system clipboard in neovim.

--- nvim/lua/default/plugins.lua
+++ nvim/lua/default/plugins.lua
@@ -64,6 +64,6 @@ require("lazy").setup({
     },
     {
         "aserowy/tmux.nvim",
-        config = function() return require("tmux").setup() end
+        config = function() return require("tmux").setup({copy_sync = { enable = false } }) end
     },
 })
