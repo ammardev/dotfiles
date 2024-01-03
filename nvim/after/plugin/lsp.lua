35d93b71fb1479ff0d581bc0580ef2288fa81026
Ammar Al-Khawaldeh <me@ammar.dev>
2024-01-03 21:53:57 +0300

    Add settings for pylsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,19 +1,42 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  lsp.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    lsp.default_keymaps({buffer = bufnr})
 end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').pylsp.setup({
+    settings = {
+        pylsp = {
+            configurationSources = {},
+            plugins = {
+                autopep8 = { enabled = false },
+                flake8 = { enabled = false },
+                yapf = { enabled = false },
+                mccabe = { enabled = false },
+                pycodestyle = { enabled = false },
+                preload = { enabled = false },
+                pyflakes = { enabled = false },
+                pylint = { enabled = false },
+                jedi_completion = {
+                    enabled = true,
+                },
+                jedi = {
+                    extra_paths = {'./pyvendor'},
+                }
+            },
+        },
+    }
+})
 
 lsp.setup()
 
 local cmp = require('cmp')
 
 cmp.setup({
-  mapping = {
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
-  }
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({select = true}),
+    }
 })
