3fd4733bff60ece7bd95c813976b315d484acf28
Ammar Al-Khawaldeh <me@ammar.dev>
2024-01-13 16:37:32 +0300

    Use appimage for nvim for non-macos

--- install.sh
+++ install.sh
@@ -98,7 +98,11 @@ function nvim_install() {
     if [ "$os" == "macos" ]; then
         install_app "NeoVim" "nvim"
     else
-        install_app "NeoVim" "neovim"
+        curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+        chmod u+x nvim.appimage
+        ./nvim.appimage --appimage-extract
+        sudo mv squashfs-root /
+        sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
     fi
 
     install_app "ripgrep" "ripgrep"
