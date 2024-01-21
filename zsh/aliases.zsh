a3d91ae2bec1a4c10dc903d2e7fd47c47511ff7e
Ammar Al-Khawaldeh <me@ammar.dev>
2024-01-21 21:08:05 +0300

    Reformat aliases file

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
+##
+## Docker Aliases
+##
 alias dc="docker-compose"
 alias ds="docker stop \$(docker ps -q)"
 alias dcu="docker-compose up -d"
@@ -13,18 +16,32 @@ function _dce() {
     fi
 }
 compdef _dce dce
-alias edit-host="sudo nvim /etc/hosts"
-alias check-port="nc -vz"
+
+##
+## VIM Aliases
+##
 alias nano="nvim"
 alias vi="nvim"
 alias vim="nvim"
 alias :q="exit"
 
-alias deploy-to-testing="git checkout develop && git fetch && git reset --hard origin/develop && git merge - && git push && git checkout -"
-alias gc='git checkout $(git branch | fzf-tmux -h)'
+##
+## Networking Aliases
+##
+alias edit-host="sudo nvim /etc/hosts"
+alias check-port="nc -vz"
 
+##
+## DigitalOcean Aliases
+##
 alias do-list="doctl compute droplet list"
 alias do-create="doctl compute droplet create --wait --ssh-keys $DO_SSH_KEY_ID --region=fra1 --image=ubuntu-23-10-x64 --size s-1vcpu-2gb"
 alias do-ssh="doctl compute ssh"
 alias do-delete="doctl compute droplet delete"
+
+##
+## Workflow Aliases
+##
+alias deploy-to-testing="git checkout develop && git fetch && git reset --hard origin/develop && git merge - && git push && git checkout -"
+alias gc='git checkout $(git branch | fzf-tmux -h)'
 
