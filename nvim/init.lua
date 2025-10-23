ce2c94c09e6853ce12ef0df2cf95e55f091adbb2
Ammar Al-Khawaldeh <me@ammar.dev>
2025-10-23 20:38:24 +0300

    Add temporary user command

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,3 +4,23 @@ require("plugins")
 
 require("gx_open")
 require("statusline")
+
+-- TMP
+vim.api.nvim_create_user_command("Today", function()
+    local year = tonumber(os.date("%Y"))
+    local month = tonumber(os.date("%m"))
+    local day = tonumber(os.date("%d"))
+    local quarter = math.floor((month - 1) / 3) + 1
+    local first_wday = tonumber(os.date("%w", os.time { year = year, month = month, day = 1 }))
+    local week_of_month = math.floor((day + first_wday) / 7) + 1
+
+    local file_path = string.format("~/Workspace/zid-worklogs/%s-Q%s/%s.W%s.md", year, quarter, os.date("%b-%d.%a"),
+        week_of_month)
+    vim.cmd("edit " .. file_path)
+end, {})
+
+vim.filetype.add({
+    filename = {
+        ["Caddyfile"] = "caddy",
+    },
+})
